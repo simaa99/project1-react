18c49e3388b7e759788dcaa0f794eca206b21266
simaa99 <semaamosa99@gmail.com>
2023-07-15 14:40:11 +0300

    create orline component

---
+++ src/components/OrLineComponent/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './style.css'
+export default class index extends Component {
+  render() {
+    return (
+      <div className='or-line'>
+        <div></div>
+        <span>Or</span>
+        <div></div>
+      </div>
+    )
+  }
+}
