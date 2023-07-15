6a8305d86bd43888072922f6167cfb7b6cf36bc9
simaa99 <semaamosa99@gmail.com>
2023-07-15 14:47:06 +0300

    create login left side

---
+++ src/sections/LoginLeftSection/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import logoLogin from "../../assets/Group 3.svg";
+import iconQuote from "../../assets/icon-quote.svg";
+import QuoteComponent from "../../components/QuoteComponent";
+import LogoComponent from "../../components/LogoComponent";
+import LoginImg from "../../assets/login-icon-end.svg";
+import "./style.css";
+export default class index extends Component {
+  render() {
+    return (
+      <div className="login-left">
+        <LogoComponent
+          logoImg={logoLogin}
+          logoTitle="Gamers"
+          isLogin={true}
+          isTitleLogin={true}
+        />{" "}
+        <div className="logo-left-bottom">
+          <QuoteComponent
+            quoteIcon={iconQuote}
+            quoteDesc="I always observe the people who pass by when I ride an escalator. I'll never see most of them again, so I imagine a lot of things about their lives... about the day ahead of them."
+            quotePerson="Hideo Kojima"
+            quoteEnd={LoginImg}
+            isLogin={true}
+          />
+        </div>
+      </div>
+    );
+  }
+}
