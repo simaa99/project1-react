9e191f87f14c826f99a08cc2fb4d64c27e913249
simaa99 <semaamosa99@gmail.com>
2023-07-15 14:43:23 +0300

    add socail data

---
+++ src/mock/social.js
@@ -0,0 +1,13 @@
+import twitter from "../assets/logos_twitter.svg";
+import google from "../assets/flat-color-icons_google.svg";
+import link from "../assets/cib_linkedin-in.svg";
+import git from "../assets/ant-design_github-filled.svg";
+
+const social = [
+  { id: 1, src:  google  },
+  { id: 2, src: twitter  },
+  { id: 3, src: link  },
+  { id: 4, src: git  },
+];
+
+export default social;
