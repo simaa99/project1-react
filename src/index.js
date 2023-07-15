3ca45b5abfe95deeaf95709d97670c63db282b0d
simaa99 <semaamosa99@gmail.com>
2023-07-15 14:29:22 +0300

    create structure task

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/App.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+  </>
+);
